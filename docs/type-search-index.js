38797f8b1d8f3e0764115dbe1cbc4a356ae34ada
MaskedPaladin <carjien@gmail.com>
2022-01-30 15:13:22 +0100

    v1.1

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"DataMinerImproved"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"UrlObtainer"}];updateSearchResults();
